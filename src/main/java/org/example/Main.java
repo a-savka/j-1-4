57aa22fcd3fcd8890d53d22d9526878f444cdad0
Andrey Savka <a_sawka@yahoo.com>
2024-02-17 17:32:20 +0300

    Feature 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Feature 1");
         for (int i = 0; i <= 10; i++) {
             System.out.println("index => " + i);
         }
